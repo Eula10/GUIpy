503dec8078c57450948ec2037a1d00ecbe28cba6
Eulalio Hernandez <sergioeulalio49@gmail.com>
2023-05-25 10:50:39 +0200

    The GUI avance well, the birds are placed in the right side of the window, then with the mouse is possible to drag & drop the birds in any position. Finally I add a right click menu to delete the image and open the propietis parameter

--- GUI.py
+++ GUI.py
@@ -1,51 +1,76 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
-class ImageObject:
-    def __init__(self, canvas, image, x, y):
+class DragDropImage:
+    def __init__(self, canvas, image_path, x, y):
         self.canvas = canvas
-        self.image = image
-        self.x = x
-        self.y = y
-        self.canvas_id = canvas.create_image(x, y, image=self.image)
-        self.canvas.tag_bind(self.canvas_id, "<Button1-Motion>", self.move)
+        self.image = Image.open(image_path).resize((100, 100))
+        self.image_tk = ImageTk.PhotoImage(self.image)
+        self.image_id = self.canvas.create_image(x, y, image=self.image_tk)
+        self.canvas.tag_bind(self.image_id, "<Button-1>", self.start_drag)
+        self.canvas.tag_bind(self.image_id, "<B1-Motion>", self.drag)
+        self.canvas.tag_bind(self.image_id, "<ButtonRelease-1>", self.drop)
+        self.canvas.tag_bind(self.image_id, "<Button-3>", self.open_menu)
+        self.is_dragging = False
+        self.offset_x = 0
+        self.offset_y = 0
 
-    def move(self, event):
-        dx = event.x - self.x
-        dy = event.y - self.y
-        self.canvas.move(self.canvas_id, dx, dy)
-        self.x = event.x
-        self.y = event.y
+    def start_drag(self, event):
+        self.is_dragging = True
+        self.offset_x = event.x - self.canvas.coords(self.image_id)[0]
+        self.offset_y = event.y - self.canvas.coords(self.image_id)[1]
 
-def center_images(event):
-    canvas_width = event.width
-    canvas_height = event.height
+    def drag(self, event):
+        if self.is_dragging:
+            x = event.x - self.offset_x
+            y = event.y - self.offset_y
+            self.canvas.coords(self.image_id, x, y)
 
-    # Calculate center coordinates
-    center_x = canvas_width // 2
-    center_y = canvas_height // 2
+    def drop(self, event):
+        if self.is_dragging:
+            self.is_dragging = False
+            x = event.x - self.offset_x
+            y = event.y - self.offset_y
+            self.animate_drop(x, y)
 
-    # Move images to center
-    canvas.coords(image_object.canvas_id, center_x, center_y)
-    canvas.coords(image_object2.canvas_id, center_x, center_y)
-    canvas.coords(image_object3.canvas_id, center_x, center_y)
+    def animate_drop(self, x, y):
+        dx = x - self.canvas.coords(self.image_id)[0]
+        dy = y - self.canvas.coords(self.image_id)[1]
+        step = 5
+        delay = 10
+        for i in range(step):
+            self.canvas.after(delay * i, lambda: self.move_image(dx/step, dy/step))
+        self.canvas.after(delay * step, lambda: self.move_image(dx - (dx/step)*step, dy - (dy/step)*step))
 
+    def move_image(self, dx, dy):
+        self.canvas.move(self.image_id, dx, dy)
+
+    def open_menu(self, event):
+        menu = tk.Menu(self.canvas, tearoff=0)
+        menu.add_command(label="Properties", command=self.properties)
+        menu.add_command(label="Delete", command=self.delete)
+        menu.post(event.x_root, event.y_root)
+
+    def properties(self):
+        # Code to handle the "Properties" option
+        pass
+
+    def delete(self):
+        self.canvas.delete(self.image_id)
+
+# Create the main window
 root = tk.Tk()
-root.title("Image Movement")
+root.title("Drag and Drop Images")
+root.geometry("400x400")
 
-canvas = tk.Canvas(root)
-canvas.pack(fill=tk.BOTH, expand=True)  # Fill the window and expand to fit
+# Create a canvas
+canvas = tk.Canvas(root, width=400, height=400)
+canvas.pack()
 
-# Load different images
-image1 = tk.PhotoImage(file="bird.png")
-image2 = tk.PhotoImage(file="bird.png")
-image3 = tk.PhotoImage(file="bird.png")
+# Create the image objects and place them on the canvas
+image1 = DragDropImage(canvas, "bird.png", 50, 50)
+image2 = DragDropImage(canvas, "bird.png", 150, 150)
+image3 = DragDropImage(canvas, "bird.png", 250, 250)
 
-# Create image objects
-image_object = ImageObject(canvas, image1, 100, 100)
-image_object2 = ImageObject(canvas, image2, 200, 200)
-image_object3 = ImageObject(canvas, image3, 300, 300)
-
-# Bind window resize event to center_images function
-root.bind("<Configure>", center_images)
-
+# Start the Tkinter event loop
 root.mainloop()
